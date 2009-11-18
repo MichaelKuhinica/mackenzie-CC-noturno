55e3d15242fe900db87ec26d5c3a577618637b8c
Taverneiro <taverneiro@gmail.com>
2009-11-18 10:33:02 -0200

    Versão final do programa dos pedreiros

--- SO/pedreiros.c
+++ SO/pedreiros.c
@@ -11,18 +11,22 @@
 
 void inicia_threads(void);
 void trabalha(void*);
-void inicializa_casas();
-void inicializa_pedreiros();
-void print_pedreiro(pedreiro_struct*);
+void inicializa_casas(void);
+void inicializa_pedreiros(void);
+void print_pedreiros(void);
+void print_estoque(void);
+void print_casas(void);
+int decide_casa(void);
+void checa_casa(casa_struct*);
 
 int rodando = 1;
 
 int main(void) {
-    /*printf("Numero de pedreiros: ");
+    printf("Numero de pedreiros: ");
     scanf("%d",&P);
     printf("Quantidade de casas a construir: ");
     scanf("%d",&C);
-    printf("Cada casa precissa de quanto cimento? ");
+    printf("Cada casa precisa de quanto cimento? ");
     scanf("%d",&Cc);
     printf("Cada casa precisa de quantos tijolos? ");
     scanf("%d",&Ct);
@@ -33,27 +37,35 @@ int main(void) {
     printf("Um peao carrega quanto de tijolo? ");
     scanf("%d",&Pt);
     printf("Quantos segundos ele demora para carregar o carrinho de tijolos? ");
-    scanf("%d",&Ptt); 
-    */
+    scanf("%d",&Ptt);
+    printf("Quantos segundos ele demora para concluir o trabalho? ");
+    scanf("%d",&Pts); 
+    
     m_casas = (pthread_mutex_t *)malloc(C * sizeof(pthread_mutex_t));
     t_pedreiros = (pthread_t *)malloc(P * sizeof(pthread_t));
     casas = (casa_struct *)malloc(C * sizeof(casa_struct));
     pedreiros = (pedreiro_struct *)malloc(P * sizeof(pedreiro_struct));
     
+    estoque_tijolos = C*Ct;
+    estoque_cimento = C*Cc;
+    
+    system("clear");
+
     inicializa_casas();
     inicializa_pedreiros();
     
-    int i;
     inicia_threads();
-    system("clear");
     while(rodando == 1) {
-        system("clear");
+        system("clear");        
         
-        for(i=0; i<P; i++) {
-            print_pedreiro(&pedreiros[i]);
-        }  
+        print_pedreiros();
+        printf("\n*************************\n");
+        print_casas();
+        printf("\n*************************\n");
+        print_estoque();
     }
     
+    printf("\nCasas prontas! =D\n");
     return 0;
 }
 
@@ -73,6 +85,7 @@ void inicializa_pedreiros() {
         pedreiros[i].cimento = 0;
         pedreiros[i].tijolos = 0;
         pedreiros[i].numero = i;
+        pedreiros[i].casa = -1;
     }
 }
 
@@ -83,24 +96,130 @@ void inicia_threads() {
  }   
 }
 
-void print_pedreiro(pedreiro_struct *p) {
-    printf("Pedreiro n%d\nCimento: %d\nTijolos: %d\nTrabalhando na casa:%d\n", p->numero, p->cimento, p->tijolos, p->casa);
+void print_estoque() {
+    printf("\nEstoque:\n");
+    printf("Cimento: \t%d\n", estoque_cimento);
+    printf("Tijolos: \t%d\n", estoque_tijolos);
+    printf("\n");
+}
+
+void print_casas() {
+    int i;
+    printf("\n\t\t");
+    for(i=0; i < C; i++) {
+        printf("Casa %d\t\t", i);
+    }
+    printf("\nCimento\t\t");
+    for(i=0; i < C; i++) {
+        printf("%d\t\t", casas[i].cimento);
+    }
+    printf("\nTijolos\t\t");
+    for(i=0; i < C; i++) {
+        printf("%d\t\t", casas[i].tijolos);
+    }
+    printf("\nPronta?   \t");
+    for(i=0; i < C; i++) {
+        if(casas[i].pronta == 0)
+            printf("Nao\t\t");
+        else
+            printf("Sim\t\t");
+    }
+    printf("\n");
+
+    
+}
+
+void print_pedreiros() {
+    int i;
+    printf("\n\t");
+    for(i=0; i < P; i++) {
+        printf("Pedreiro %d\t", i);
+    }
+    printf("\nCimento\t\t");
+    for(i=0; i < P; i++) {
+        printf("%d\t\t", pedreiros[i].cimento);
+    }
+    printf("\nTijolos\t\t");
+    for(i=0; i < P; i++) {
+        printf("%d\t\t", pedreiros[i].tijolos);
+    }
+    printf("\nCasa   \t\t");
+    for(i=0; i < P; i++) {
+        if(pedreiros[i].casa < 0)
+            printf("Nenhuma\t\t");
+        else
+            printf("%d\t\t", pedreiros[i].casa);
+    }
+    printf("\n");
 }
 
 void trabalha(void *ped) {
-    pedreiro_struct *p;
-    p = (pedreiro_struct *) ped;
-    pthread_mutex_lock( &m_cimento );
-    sleep(Ptc);
-    p->cimento = Pc;
-    pthread_mutex_unlock( &m_cimento );
+    int qtd_cimento;
+    int qtd_tijolos;
+    int tijolos_precisa;
+    int cimento_precisa;
+    while(rodando == 1) {
+                                        
+        pedreiro_struct *p;
+        p = (pedreiro_struct *) ped;
+        pthread_mutex_lock( &m_cimento );
+        qtd_cimento = Pc - p->cimento;
+        if((estoque_cimento - qtd_cimento) >= 0) {
+            estoque_cimento = estoque_cimento - qtd_cimento;
+            p->cimento = p->cimento + qtd_cimento;
+            sleep(Ptc);
+        }            
+        pthread_mutex_unlock( &m_cimento );
 
-    pthread_mutex_lock( &m_tijolos );
-    sleep(Ptt);
-    p->tijolos = Pt;
-    pthread_mutex_unlock( &m_tijolos );
-
+        pthread_mutex_lock( &m_tijolos );
+        qtd_tijolos = Pt - p->tijolos;
+        if((estoque_tijolos - qtd_tijolos) >= 0) {
+            estoque_tijolos =  estoque_tijolos - qtd_tijolos;
+            p->tijolos = p->tijolos + qtd_tijolos;
+            sleep(Ptt);
+        }
+        pthread_mutex_unlock( &m_tijolos );
     
-    
+        int casa;
+        casa = decide_casa();
+        if(casa == -1)
+            rodando = 0;
+        else {
+            pthread_mutex_lock((pthread_mutex_t*) &m_casas[casa]);
+            p->casa = casa;
+            
+            tijolos_precisa = Ct - casas[casa].tijolos;
+            cimento_precisa = Cc - casas[casa].cimento;
+            
+            if(tijolos_precisa > p->tijolos)
+                tijolos_precisa = p->tijolos;
+            if(cimento_precisa > p->cimento)
+                cimento_precisa = p->cimento;
+            
+            casas[casa].cimento = casas[casa].cimento+cimento_precisa;
+            casas[casa].tijolos = casas[casa].tijolos+tijolos_precisa;
+            
+            p->cimento = p->cimento - cimento_precisa;
+            p->tijolos = p->tijolos - tijolos_precisa;
+            
+            sleep(Pts);
+            p->casa = -1;
+            checa_casa(&casas[casa]);
+            pthread_mutex_unlock((pthread_mutex_t*) &m_casas[casa]);        
+        }
+    }
 }
 
+int decide_casa() {
+    int i;
+    for(i=0; i<C; i++) {
+        if(casas[i].pronta == 0)
+            return i;
+    }    
+    return -1;
+}
+
+void checa_casa(casa_struct* c) {
+    if(c->cimento >= Cc && c->tijolos >= Ct)
+        c->pronta = 1;
+}
--- SO/vars.c
+++ SO/vars.c
@@ -14,14 +14,18 @@ typedef struct {
 } pedreiro_struct;
 
 //variaveis obrigatorias do exercicio
-static int C=10;                         //indica o numero de casas
-static int P=3;                         //indica o numero de pedreiros
-static int Cc=100;                        //indica quantos quilos de cimento cada casa precisa
-static int Ct=200;                        //indica quantos tijolos para a contrução de cada casas
+static int C=3;                         //indica o numero de casas
+static int P=2;                            //indica o numero de pedreiros
+static int Cc=10;                        //indica quantos quilos de cimento cada casa precisa
+static int Ct=20;                        //indica quantos tijolos para a contrução de cada casas
 static int Pc=5;                        //indica quantos quilos de cimento o peao pode carregar
-static int Ptc=2;                       //indica quanto tempo para colocar Pc no carrinho
+static int Ptc=1;                       //indica quanto tempo para colocar Pc no carrinho
 static int Pt=10;                        //indica quantos tijolos o peao consegue carregar
-static int Ptt=5;                       //indica quanto tempo demora para colocar Pt no carrinho
+static int Ptt=1;                       //indica quanto tempo demora para colocar Pt no carrinho
+static int Pts=3;                       // quanto tempo o pedreiro demora trabalhando
+
+int estoque_tijolos;
+int estoque_cimento; 
 
 pthread_mutex_t m_tijolos = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t m_cimento = PTHREAD_MUTEX_INITIALIZER;
